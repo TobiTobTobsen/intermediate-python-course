2d637a60099a5dbe091204adf728ccbeb1b2f030
TobiTobTobsen <tobsen85@outlook.de>
2020-08-05 21:12:36 +0200

    supply dice sum and function

--- dice_roller.py
+++ dice_roller.py
@@ -1,9 +1,19 @@
 #import modul random
 import random
+#add dice rolls numbers
+dice_rolls = 2
+#add sum
+dice_sum = 0
 
 def main():
   roll = random.randint(1,6)
-  print(f"you rolled a {roll}")
 
-if __name__== "__main__":
-  main()
+  print(f"you rolled a {roll}")
+  return roll
+
+for i in range (0,dice_rolls):
+  if __name__== "__main__":
+    dice_sum = dice_sum + main()
+
+#print sum
+print(f'You have rolled a total of {dice_sum}')
